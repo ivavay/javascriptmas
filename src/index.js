ed4c912ca366350eb304303c69612f3efe2dc9b8
Ivy Chen <36115038+ivavay@users.noreply.github.com>
2021-12-03 17:39:39 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import "./styles.css";
+
+const images = [
+  "../images/day1.jpg",
+  "../images/day2.jpg",
+  "../images/day3.jpg"
+];
+
+let i = 0;
+
+function addImage(x, y) {
+  const nextImage = images[i];
+  const img = document.createElement("img");
+  img.setAttribute("src", nextImage);
+  img.style.left = x + "px";
+  img.style.top = y + "px";
+
+  document.body.appendChild(img);
+  i = i + 1;
+
+  if (i > images.length) {
+    i = 0;
+  }
+
+  const dayDisplay = document.getElementById("day");
+
+  if (images[i - 1]) {
+    dayDisplay.textContent = "Day" + " " + i;
+    console.log(i - 1);
+    console.log(images[i - 1]);
+  }
+}
+
+document.addEventListener("click", function (event) {
+  event.preventDefault();
+  addImage(event.pageX, event.pageY);
+});
